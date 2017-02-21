4ab26a22e8f7f18f621361652b45218ccb57364e
Rhys Bradbury <iRhysBradbury@users.noreply.github.com>
2017-02-21 14:37:57 +0000

    Revert "Feature/model"

--- app/com/capgemini/shopping/services/ProductService.scala
+++
@@ -1,7 +0,0 @@
-package com.capgemini.shopping.services
-
-import javax.inject.Singleton
-import com.capgemini.shopping.model.ProductDefinitions
-
-@Singleton
-class ProductService extends ProductDefinitions
