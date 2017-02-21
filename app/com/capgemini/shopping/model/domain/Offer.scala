453b1f8ab06d8cb5bdf8231f811a414a20ecf2c3
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-21 16:36:08 +0000

    applying offers to calc func

--- app/com/capgemini/shopping/model/domain/Offer.scala
+++ app/com/capgemini/shopping/model/domain/Offer.scala
@@ -17,7 +17,6 @@ object Offer {
     productGroup.take(amountOfGroups + remainder)
   }
 
-
   private def filterOnProductName(products: Seq[Product], productName: String): Seq[Product] = {
     products.filter(_.name == productName)
   }
