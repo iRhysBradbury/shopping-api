bfdcae58f02bae3bfb6d910f7bcff523512ec94c
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-21 14:30:35 +0000

    adding product model

---
+++ app/com/capgemini/shopping/controllers/ShoppingController.scala
@@ -0,0 +1,15 @@
+package com.capgemini.shopping.controllers
+
+import javax.inject._
+import play.api.mvc._
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class ShoppingController @Inject()(implicit exec: ExecutionContext) extends Controller {
+
+  def getProducts(): Action[AnyContent] = Action {
+  }
+
+  def
+
+}
